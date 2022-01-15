9976b07fe5dab328c7e9853b3beb2b86ef090a88
Abdumalik Khojimirzaev <hojimirzaevabdumalik@gmail.com>
2022-01-15 17:11:37 +0500

    Initial Commit

---
+++ src/config/config.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+interface Config {
+    HttpPort: string
+    MongoPort: number
+    MongoDatabase: string
+    NodeEnv: string
+}
+
+let config: Config = {
+    HttpPort: getConf('HTTP_PORT', '3000'),
+    MongoPort: parseInt(getConf('MONGO_PORT', '27017')),
+    MongoDatabase: getConf('MONGO_DATABASE', 'sample_project'),
+    NodeEnv: getConf('NODE_ENV', 'development')
+}
+
+function getConf(name: string, def: string = ''): string {
+    if (process.env[name]) {
+        return process.env[name] || ''
+    }
+
+    return def
+}
+
+export default config
